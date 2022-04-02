8ef12fd64f58c054c30c9e53b81047c9b51885e0
olfed <jdwop@mail.ru>
2022-04-02 17:53:36 +0300

    Client.java and Server.java now work correctly

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,9 @@
-import java.io.*;
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
